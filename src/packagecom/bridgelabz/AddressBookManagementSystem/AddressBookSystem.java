e43de0a67cf14b0b9a1dab3204e8f81311da6db5
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-01-09 03:54:43 -0800

    Address Book

---
+++ src/packagecom/bridgelabz/AddressBookManagementSystem/AddressBookSystem.java
@@ -0,0 +1,11 @@
+package packagecom.bridgelabz.AddressBookManagementSystem;
+
+public class AddressBookSystem {
+    public static void main(String[] args)
+    {
+        System.out.println("****** Welcome To The Address Book Program ******");
+//            addContact();
+//            System.out.println(contactList);
+    }
+
+}
